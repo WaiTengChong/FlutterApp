4ee3b2734538f9f86cb3ff13786267b7237019d7
Tony Chong <waitengchong12@gmail.com>
2020-09-08 00:49:10 +0100

    WIP SignUp
    Added email and gender selection

--- lib/pages/Signup.dart
+++ lib/pages/Signup.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 
-class Signup extends StatelessWidget {
+class Signup extends StatefulWidget {
   static const String routeName = "/Signup";
+
+  @override
+  _SignupState createState() => new _SignupState();
+}
+
+int gender = 1;
+int group;
+
+class _SignupState extends State<Signup> {
+  TextEditingController userNameController,
+      passwordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -65,6 +75,65 @@ class Signup extends StatelessWidget {
                         ),
                         //controller: passwordController,
                       )),
+                  Container(
+                      margin: const EdgeInsets.only(top: 30),
+                      child: TextField(
+                        autofocus: false,
+                        style:
+                            TextStyle(fontSize: 22.0, color: Color(0xFFbdc6cf)),
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: Colors.white,
+                          hintText: 'Email',
+                          contentPadding: const EdgeInsets.only(
+                              left: 14.0, bottom: 8.0, top: 8.0),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(25.7),
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                            borderRadius: BorderRadius.circular(25.7),
+                          ),
+                        ),
+                        //controller: emailController,
+                      )),
+                  Container(
+                    height: 30.0,
+                    margin: const EdgeInsets.only(top: 30),
+                    width: double.infinity,
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      children: <Widget>[
+                        Text("Male",
+                            style:
+                                TextStyle(fontSize: 25, color: Colors.white)),
+                        Padding(padding: EdgeInsets.all(5.00)),
+                        Radio(
+                            value: 1,
+                            groupValue: group,
+                            onChanged: (int e) {
+                              print(e);
+                              setState(() {
+                                group = e;
+                              });
+                            }),
+                        Padding(padding: EdgeInsets.all(5.00)),
+                        Text("Female",
+                            style:
+                                TextStyle(fontSize: 25, color: Colors.white)),
+                        Radio(
+                            value: 0,
+                            groupValue: group,
+                            onChanged: (int e) {
+                              print(e);
+                              setState(() {
+                                group = e;
+                              });
+                            }),
+                      ],
+                    ),
+                  ),
                   Container(
                       margin: const EdgeInsets.only(top: 30),
                       child: FlatButton.icon(
